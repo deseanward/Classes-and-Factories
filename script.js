f7bd9b2113a6e9276986cac8ad9cf9e3db064353
De Sean Ward <dward1711@gmail.com>
2023-07-11 13:35:34 -0500

    Person store created

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ class Hamster {
 class Person {
 	constructor(
 		name,
-		age = '0',
-		height = '0',
-		weight = '0',
+		age = 0,
+		height = 0,
+		weight = 0,
 		mood = 0,
 		hamsters = [],
-		bankAccount = '0'
+		bankAccount = 0
 	) {
 		(this.name = name),
 			(this.age = age),
@@ -57,26 +57,51 @@ class Person {
 		return `Hello, ${name}`;
 	}
 
-	eat() {
-		weight++;
-		mood++;
+	eat(num) {
+		for (let x = 0; x < num; x++) {
+			this.weight++;
+			this.mood++;
+		}
 	}
 
-	exercise() {
-		weight--;
+	exercise(num) {
+		for (let x = 0; x < num; x++) {
+			this.weight--;
+		}
 	}
 
-	ageUp() {
-		age++;
-		height++;
-		weight++;
-		mood--;
-		bankAccount += 10;
+	ageUp(num) {
+		for (let x = 0; x < num; x++) {
+			this.age++;
+			this.height++;
+			this.weight++;
+			this.mood--;
+			this.bankAccount += 10;
+		}
 	}
 
 	buyHamster(hamster) {
 		this.hamsters.push(hamster);
-		mood += 10;
-		bankAccount -= hamster.getPrice();
+		this.mood += 10;
+		this.bankAccount -= hamster.getPrice();
 	}
 }
+
+const timmy = new Person('Timmy');
+timmy.ageUp(5);
+
+timmy.eat(5);
+timmy.exercise(5);
+timmy.ageUp(9);
+
+const gus = new Hamster();
+gus.name - 'Gus';
+gus.owner = 'Timmy';
+
+timmy.buyHamster(gus);
+timmy.ageUp(15);
+
+timmy.eat(2);
+timmy.exercise(2);
+
+console.log(timmy);
