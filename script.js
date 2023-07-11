965b8a72e098bee2316d74536ab65cab0a350862
De Sean Ward <dward1711@gmail.com>
2023-07-11 12:24:01 -0500

    Person class created

--- script.js
+++ script.js
@@ -1,23 +1,82 @@
 /**
- * ******************** HAMSTER ********************
+ /* ******************** HAMSTER ********************
  */
 class Hamster {
-    constructor(owner = '', name, price = 15) {
-        this.owner = owner,
-        this.name = name,
-        this.price =price
-    }
+	constructor(owner = '', name, price = 15) {
+		(this.owner = owner), (this.name = name), (this.price = price);
+	}
 
-    wheelRun() {
-        console.log('squeak squeak')
-    }
+	wheelRun() {
+		console.log('squeak squeak');
+	}
 
-    eatFood() {
-        console.log('nibble nibble');
-    }
+	eatFood() {
+		console.log('nibble nibble');
+	}
 
-    getPrice() {
-        return this.price;
-    }
+	getPrice() {
+		return this.price;
+	}
+}
 
+/**
+ * ******************** PERSON ********************
+ */
+class Person {
+	constructor(
+		name,
+		age = '0',
+		height = '0',
+		weight = '0',
+		mood = 0,
+		hamsters = [],
+		bankAccount = '0'
+	) {
+		(this.name = name),
+			(this.age = age),
+			(this.height = height),
+			(this.weight = weight),
+			(this.mood = mood),
+			(this.hamsters = hamsters),
+			(this.bankAccount = bankAccount);
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getAge() {
+		return this.age;
+	}
+
+	getWeight() {
+		return this.weight;
+	}
+
+	greet(name) {
+		return `Hello, ${name}`;
+	}
+
+	eat() {
+		weight++;
+		mood++;
+	}
+
+	exercise() {
+		weight--;
+	}
+
+	ageUp() {
+		age++;
+		height++;
+		weight++;
+		mood--;
+		bankAccount += 10;
+	}
+
+	buyHamster(hamster) {
+		this.hamsters.push(hamster);
+		mood += 10;
+		bankAccount -= hamster.getPrice();
+	}
 }
